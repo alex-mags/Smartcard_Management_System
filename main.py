15fdfe436657f18836a8a875b3cb8a5d98f536ac
Alex Magson <alexemagson@gmail.com>
2026-06-02 15:23:09 +0100

    new date format

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def main():
             remove_card(input("Card number to remove: "))
 
         elif choice == "4":
-            print("Example: SELECT * FROM cards WHERE holder_name like 'robin%'")
+            print("Example: SELECT * FROM cards WHERE holder_name like 'name%'")
             query = input("Enter SQL query: ")
             rows = search_cards(query)
             if rows:
